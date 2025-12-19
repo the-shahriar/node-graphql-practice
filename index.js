e3700d8a6bcbb971f8f9a4a9f4b9118f6bc08549
islam-shahriar <shahriar.islam@apsissolutions.com>
2025-12-19 13:23:50 +0800

    connected graphql with dummy product

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import schema from "./data/schema";
 
 const app = express();
 const PORT = 8080
@@ -7,4 +9,21 @@ app.get('/', (req, res)=> {
     res.send('GraphQL is awesome')
 })
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+const root = { product: () => {
+    return {
+        "id": 202592303777374,
+        "name": 'Baby Soap',
+        "description": 'This is a baby product',
+        "price": 100.99,
+        "soldout": false
+    }
+} };
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}))
+
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}/graphql`));
